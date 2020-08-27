1bb0af2e7dd0bc14435919efadc310076eb351b5
Janakarajan Natarajan <janakan@amazon.com>
2020-08-27 18:56:10 +0000

    Change build-wheels for x86 and aarch64

--- build-wheels.sh
+++ build-wheels.sh
@@ -8,6 +8,14 @@ yum update -y
 /opt/python/cp39-cp39/bin/python3.9 /io/setup.py bdist_wheel
 cp /io/dist/*.whl /io/
 for whl in /io/*_aarch64.whl; do
-	auditwheel repair $whl --plat "manylinux2014_aarch64" -w /io/
+	if [ `uname -m` = 'aarch64' ]; then
+		auditwheel repair $whl --plat "manylinux2014_aarch64" -w /io/
+	else
+		auditwheel repair $whl --plat "manylinux2014_x86_64" -w /io/
+	fi
 done
-/opt/python/cp39-cp39/bin/pip3.9 install /io/*-manylinux2014_aarch64.whl
+if [ `uname -m` = 'aarch64' ]; then
+	/opt/python/cp39-cp39/bin/pip3.9 install /io/*-manylinux2014_aarch64.whl
+else
+	/opt/python/cp39-cp39/bin/pip3.9 install /io/*-manylinux2014_x86_64.whl
+fi
