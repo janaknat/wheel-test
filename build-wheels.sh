7527547344a47fb783e036ea4983920075dd0f0b
Janakarajan Natarajan <janakan@amazon.com>
2020-08-27 19:00:54 +0000

    Typo

--- build-wheels.sh
+++ build-wheels.sh
@@ -7,7 +7,7 @@ mkdir /io/wheelhouse
 yum update -y
 /opt/python/cp39-cp39/bin/python3.9 /io/setup.py bdist_wheel
 cp /io/dist/*.whl /io/
-for whl in /io/*_aarch64.whl; do
+for whl in /io/*.whl; do
 	if [ `uname -m` = 'aarch64' ]; then
 		auditwheel repair $whl --plat "manylinux2014_aarch64" -w /io/
 	else
